8abbf6a23ff5bf373ee36d546ab0b31cd75b5d0f
mohamed hadad <hadadmohamed309@gmail.com>
2025-08-17 02:48:12 +0300

    first commit

---
+++ fff.js
@@ -0,0 +1,9 @@
+git init //creat git to file
+explorer.exe  // open file inside
+code. //open code in vs
+git add namefile //add file in git
+git add . or * // add all files
+git status //show file add
+git commit -m "massage" // creat commit to file new add git
+git config --global user
+git commit-am"" // max togit add and git commit
